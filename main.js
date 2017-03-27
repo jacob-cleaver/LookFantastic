70a870741da5fe622af4b7d35331c5e9299457ff
jacob cleaver <jacob.cleaver@go.edgehill.ac.uk>
2017-03-27 18:09:03 +0100

    LookFabulous repo cloned and edited for GainX
    Basic layout used with Sass using Bootstrap styled col grid

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-$(function() {
-    $("[name=checkout]").click(function() {
-        $('.hideCheckout').hide();
-        $("#checkout-" + $(this).val()).show();
-    });
-});
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
